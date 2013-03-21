a1cc9084c7e2b4a5cce7b086e5e0ee6a5789edb3
Dominik "Socek" Długajczyk <msocek@gmail.com>
2013-03-21 18:24:08 +0100

    Init commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(name='SmallSettings',
+          version='0.1',
+          author=['Dominik "Socek" Długajczy'],
+          author_email=['msocek@gmail.com', ],
+          packages=find_packages(),
+          )
